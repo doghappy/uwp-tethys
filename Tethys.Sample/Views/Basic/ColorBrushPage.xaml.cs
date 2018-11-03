bfb59ac91478980ee0beefd0c954a8983abdf0d4
HeroWong <hero_wong@outlook.com>
2018-11-03 17:31:59 +0800

    button

--- Tethys.Sample/Views/Basic/ColorBrushPage.xaml.cs
+++ Tethys.Sample/Views/Basic/ColorBrushPage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace Tethys.Sample.Views.Basic
 {
-    public sealed partial class ColorBrushPage : Page,INavTitle, IDetailPage
+    public sealed partial class ColorBrushPage : Page, INavTitle, IDetailPage
     {
         public ColorBrushPage()
         {
@@ -23,11 +11,11 @@ namespace Tethys.Sample.Views.Basic
             GlyphFontFamily = new FontFamily("Segoe MDL2 Assets");
         }
 
-        public string NavTitile => "ColorBrush";
+        public string NavTitile => "ColorBrushs";
 
-        public string TitleName => "ColorBrush";
+        public string TitleName => "ColorBrushs";
 
-        public string Description => "ColorBrush like Bootstrap";
+        public string Description => "ColorBrushs like Bootstrap";
 
         public string Glyph => "\ue790";
 
